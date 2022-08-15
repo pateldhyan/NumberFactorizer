3bace1edf44ec826a329c00c6da0f92c79854265
pateldhyan <43506924+dannymanny101y@users.noreply.github.com>
2022-08-15 12:29:15 -0700

    Added factor+output function. added gitignore

--- Number Factorizer/FactorsOfANumber.cpp
+++ Number Factorizer/FactorsOfANumber.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 vector<int> Factorize(int input);
+void OutputVec(vector<int> vec);
 
 int main()
 {
@@ -16,10 +18,42 @@ int main()
 
     factors = Factorize(input);
 
+    OutputVec(factors);
+
     return 0;  
 }
 
-vector<int> Factorize(int input){
+vector<int> Factorize(int workingNum){
+    vector<int> factorVec;
+    while (workingNum % 2 == 0){        //Divides by 2 as much as possible
+        workingNum /= 2;
+        factorVec.push_back(2);         
+    }                                   //Resulting number must be odd
 
+    for(int i = 3; i <= sqrt(workingNum) ;){
+        if (workingNum % i == 0){
+            workingNum /= i;
+            factorVec.push_back(i);
+        }
+        else{
+            i = i+2;
+        }
+    }
+    factorVec.push_back(workingNum);
 
+    return factorVec;
 }
+
+void OutputVec(vector<int> vec){
+    cout << "Factors: " << endl;
+    if(vec.size() == 1){
+        cout << vec.at(0) << " is prime." << endl; 
+    }
+    else{
+        cout << vec.at(0);
+        for (int i = 1; i < vec.size(); i++){
+            cout << " * " << vec.at(i);
+        }
+    }
+}
+
