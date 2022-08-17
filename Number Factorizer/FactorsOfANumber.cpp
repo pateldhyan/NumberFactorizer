3f7a385edafc85df2bab809473d280cb9263464b
pateldhyan <43506924+dannymanny101y@users.noreply.github.com>
2022-08-17 16:27:03 -0700

    final adjustments to functions, documentation

--- Number Factorizer/FactorsOfANumber.cpp
+++ Number Factorizer/FactorsOfANumber.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+//Function signatures
 vector<int> Factorize(int input);
 void OutputVec(vector<int> vec);
 
@@ -23,20 +24,21 @@ int main()
     return 0;  
 }
 
+//Function for find factors and add them to array
 vector<int> Factorize(int workingNum){
     vector<int> factorVec;
-    while (workingNum % 2 == 0){        //Divides by 2 as much as possible
+    while (workingNum % 2 == 0){                //Divides by 2 as much as possible
         workingNum /= 2;
         factorVec.push_back(2);         
-    }                                   //Resulting number must be odd
+    }                                           //Resulting number must be odd
 
-    for(int i = 3; i <= sqrt(workingNum) ;){
-        if (workingNum % i == 0){
+    for(int i = 3; i <= sqrt(workingNum) ;){    //Goes through every number between 3 and sqrt of input
+        if (workingNum % i == 0){               // to check if there are any possible factors
             workingNum /= i;
             factorVec.push_back(i);
         }
         else{
-            i = i+2;
+            i = i+2;                            //Iterates by 2 because number must be odd
         }
     }
     factorVec.push_back(workingNum);
@@ -44,12 +46,13 @@ vector<int> Factorize(int workingNum){
     return factorVec;
 }
 
+//Function to output factors from vector
 void OutputVec(vector<int> vec){
-    cout << "Factors: " << endl;
-    if(vec.size() == 1){
+    if(vec.size() == 1){                            //Different output if input is already prime
         cout << vec.at(0) << " is prime." << endl; 
     }
     else{
+        cout << "Factors: " << endl;                //Normal output, iterates through vector
         cout << vec.at(0);
         for (int i = 1; i < vec.size(); i++){
             cout << " * " << vec.at(i);
